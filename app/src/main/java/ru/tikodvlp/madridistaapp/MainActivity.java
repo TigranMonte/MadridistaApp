e099da38808b221b9ea597ac1f1ee9c61ec7d053
Tigran <tigranma@gmail.com>
2021-12-23 17:15:35 +0300

    Madridista App - Sign In activity with animations

--- app/src/main/java/ru/tikodvlp/madridistaapp/MainActivity.java
+++ app/src/main/java/ru/tikodvlp/madridistaapp/MainActivity.java
@@ -2,9 +2,12 @@ package ru.tikodvlp.madridistaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -38,9 +41,14 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, TopLevelActivity.class);
-                startActivity(intent);
-                finish();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(ivLogo, "logo_image");
+                pairs[1] = new Pair<View, String>(tvWelcome, "logo_text");
+
+                ActivityOptions options = ActivityOptions.
+                        makeSceneTransitionAnimation(MainActivity.this, pairs);
+                startActivity(intent, options.toBundle());
             }
         },SPLASH_SCREEN);
     }
