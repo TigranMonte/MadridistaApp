d9b28eb9c117a389428219f3f032eacff62c2d25
Tigran <tigranma@gmail.com>
2022-02-11 15:52:44 +0300

    Madridista app - Quiz Activity (Countdown timer)

--- app/src/main/java/ru/tikodvlp/madridistaapp/quiz/QuizActivity.java
+++ app/src/main/java/ru/tikodvlp/madridistaapp/quiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -17,12 +18,14 @@ import androidx.appcompat.widget.Toolbar;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import ru.tikodvlp.madridistaapp.R;
 
 public class QuizActivity extends AppCompatActivity {
 
     public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
 
     private TextView tvScore;
     private TextView tvQuestionCount;
@@ -36,6 +39,11 @@ public class QuizActivity extends AppCompatActivity {
     Button btnConfirmNext;
 
     private ColorStateList textColorDefaultRb;
+    private ColorStateList textColorDefaultCd;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
     Toolbar toolbar;
 
     private List<Question> questionList;
@@ -66,6 +74,7 @@ public class QuizActivity extends AppCompatActivity {
         btnConfirmNext = findViewById(R.id.button_confirm_next);
 
         textColorDefaultRb = rb1.getTextColors();
+        textColorDefaultCd = tvCountDown.getTextColors();
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -117,13 +126,49 @@ public class QuizActivity extends AppCompatActivity {
             tvQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
             answered = false;
             btnConfirmNext.setText("Confirm");
+
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
         } else {
             finishQuiz();
         }
     }
 
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMillis = l;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                checkAnswer();
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        tvCountDown.setText(timeFormatted);
+
+        if (timeLeftInMillis < 10000) {
+            tvCountDown.setTextColor(Color.RED);
+        } else {
+            tvCountDown.setTextColor(textColorDefaultCd);
+        }
+    }
+
     private void checkAnswer() {
         answered = true;
+
+        countDownTimer.cancel();
 
         RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
         int answerNr = rbGroup.indexOfChild(rbSelected) + 1;
@@ -187,5 +232,13 @@ public class QuizActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
