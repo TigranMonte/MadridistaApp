ba2bb888a8f6b9db90503b4d8ea646dfa316375e
Tigran <tigranma@gmail.com>
2022-02-25 16:56:19 +0300

    Madridista app - Feedback activity (layout)

---
+++ app/src/main/java/ru/tikodvlp/madridistaapp/support/FeedbackActivity.java
@@ -0,0 +1,16 @@
+package ru.tikodvlp.madridistaapp.support;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.tikodvlp.madridistaapp.R;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
