90d3c97e054ae8ca77db927a541d8631b1599a90
Tigran <tigranma@gmail.com>
2022-02-28 16:46:48 +0300

    Madridista app - Feedback activity (fixed and checked)

--- app/src/main/java/ru/tikodvlp/madridistaapp/support/FeedbackActivity.java
+++ app/src/main/java/ru/tikodvlp/madridistaapp/support/FeedbackActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -14,8 +15,12 @@ import ru.tikodvlp.madridistaapp.R;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    private static final String EXTRA_EMAIL_FEEDBACK = "tigranma@gmail.com";
+    private static final String EXTRA_EMAIL_FEEDBACK_SUBJECT = "Feedback from app";
+
     TextInputLayout editTextName, editTextFeedback;
     Button btnSendFeedback;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +30,22 @@ public class FeedbackActivity extends AppCompatActivity {
         editTextFeedback = findViewById(R.id.editTextFeedback);
         editTextName = findViewById(R.id.editTextName);
         btnSendFeedback = findViewById(R.id.btnSendFeedback);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Give us a feedback");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         btnSendFeedback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("message/html");
-                intent.putExtra(Intent.EXTRA_EMAIL, new String("tigranma@gmail.com"));
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback from App");
-                intent.putExtra(Intent.EXTRA_TEXT, "Name: " + editTextName.getEditText() +
-                        "\n Message: " + editTextFeedback.getEditText());
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {EXTRA_EMAIL_FEEDBACK});
+                intent.putExtra(Intent.EXTRA_SUBJECT, EXTRA_EMAIL_FEEDBACK_SUBJECT);
+                intent.putExtra(Intent.EXTRA_TEXT, "Name: " + editTextName.getEditText().getText().toString() +
+                        "\n Message: " + editTextFeedback.getEditText().getText().toString());
                 try {
                     startActivity(Intent.createChooser(intent, "Please select Email"));
                 } catch (android.content.ActivityNotFoundException e) {
